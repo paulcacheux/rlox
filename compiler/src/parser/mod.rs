f4e2c61c4b155370b5fc4614f1895fa8cadf377c
Paul Cacheux <paulcacheux@gmail.com>
2021-07-31 22:55:40 +0200

    Improve parser

--- compiler/src/parser/mod.rs
+++ compiler/src/parser/mod.rs
@@ -16,6 +16,33 @@ pub struct Parser<'c, 's> {
     errors: Vec<ParseError>,
 }
 
+macro_rules! parse_expr_fn {
+    ($fn_name:ident, $sub_fn:ident, $($tok:pat => $op:expr,)+) => {
+        fn $fn_name(&mut self) -> Result<pt::Expression, ParserErrorCommand> {
+            let mut lhs = self.$sub_fn()?;
+
+            loop {
+                let next_st = self.peek_token(true)?;
+                let operator = match next_st.token {
+                    $($tok => $op,)+
+                    _ => break,
+                };
+
+                self.advance_lexer();
+                let rhs = self.$sub_fn()?;
+                lhs = pt::Expression::Binary(pt::BinaryExpression {
+                    operator,
+                    operator_span: next_st.span,
+                    lhs: Box::new(lhs),
+                    rhs: Box::new(rhs),
+                });
+            }
+
+            Ok(lhs)
+        }
+    };
+}
+
 impl<'c, 's> Parser<'c, 's> {
     pub fn new(lexer: ParserLexer<'c, 's>) -> Self {
         Parser {
@@ -62,7 +89,56 @@ impl<'c, 's> Parser<'c, 's> {
     }
 
     pub fn parse_expression(&mut self) -> Result<pt::Expression, ParserErrorCommand> {
-        unimplemented!()
+        self.parse_logic_or()
+    }
+
+    parse_expr_fn!(parse_logic_or, parse_logic_and,
+        Token::OrKeyword => pt::BinaryOperator::LogicalOr,
+    );
+
+    parse_expr_fn!(parse_logic_and, parse_equality,
+        Token::AndKeyword => pt::BinaryOperator::LogicalAnd,
+    );
+
+    parse_expr_fn!(parse_equality, parse_comparison,
+        Token::BangEqual => pt::BinaryOperator::NotEqual,
+        Token::EqualEqual => pt::BinaryOperator::Equal,
+    );
+
+    parse_expr_fn!(parse_comparison, parse_term,
+        Token::LessThan => pt::BinaryOperator::LessThan,
+        Token::LessOrEqual => pt::BinaryOperator::LessOrEqual,
+        Token::GreaterThan => pt::BinaryOperator::GreaterThan,
+        Token::GreaterOrEqual => pt::BinaryOperator::GreaterOrEqual,
+    );
+
+    parse_expr_fn!(parse_term, parse_factor,
+        Token::Minus => pt::BinaryOperator::Substract,
+        Token::Plus => pt::BinaryOperator::Add,
+    );
+
+    fn parse_factor(&mut self) -> Result<pt::Expression, ParserErrorCommand> {
+        let mut lhs = self.parse_unary()?;
+
+        loop {
+            let next_st = self.peek_token(true)?;
+            let operator = match next_st.token {
+                Token::Slash => pt::BinaryOperator::Divide,
+                Token::Star => pt::BinaryOperator::Multiply,
+                _ => break,
+            };
+
+            self.advance_lexer();
+            let rhs = self.parse_unary()?;
+            lhs = pt::Expression::Binary(pt::BinaryExpression {
+                operator,
+                operator_span: next_st.span,
+                lhs: Box::new(lhs),
+                rhs: Box::new(rhs),
+            });
+        }
+
+        Ok(lhs)
     }
 
     fn parse_unary(&mut self) -> Result<pt::Expression, ParserErrorCommand> {
--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,22 +1,19 @@
-use compiler::{
-    lexer::{Lexer, Token},
-    CompilationContext,
-};
+use compiler::{lexer::Lexer, parser::Parser, CompilationContext};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input_path = std::env::args().nth(1).expect("Failed to get input path");
     let content = std::fs::read_to_string(&input_path)?;
 
     let context = CompilationContext::default();
-    let mut lexer = Lexer::new(&context, &content);
+    let lexer = Lexer::new(&context, &content);
 
-    loop {
-        let st = lexer.next_token()?;
-        println!("{:?}", st);
-        if st.token == Token::EndOfFile {
-            break;
-        }
-    }
+    let mut parser = Parser::new(lexer.peekable());
+
+    let expr = parser
+        .parse_expression()
+        .expect("Failed to parse expression");
+
+    println!("{:#?}", expr);
 
     Ok(())
 }
